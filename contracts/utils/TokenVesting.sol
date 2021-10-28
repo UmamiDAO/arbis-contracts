287f742d78964f60a99ed2d86c9530b4888340a5
0xPuffin <85851308+0xPuffin@users.noreply.github.com>
2021-10-28 16:40:48 -0400

    update solidity version of vesting contract

--- contracts/utils/TokenVesting.sol
+++ contracts/utils/TokenVesting.sol
@@ -1,6 +1,4 @@
-/* solium-disable security/no-block-members */
-
-pragma solidity ^0.4.24;
+pragma solidity ^0.8.0;
 
 /**
  * @title ERC20 interface
@@ -37,78 +35,12 @@ interface IERC20 {
 
 
 /**
- * @title SafeMath
- * @dev Math operations with safety checks that revert on error
- */
-library SafeMath {
-
-  /**
-  * @dev Multiplies two numbers, reverts on overflow.
-  */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
-    // benefit is lost if 'b' is also tested.
-    // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
-    if (a == 0) {
-      return 0;
-    }
-
-    uint256 c = a * b;
-    require(c / a == b);
-
-    return c;
-  }
-
-  /**
-  * @dev Integer division of two numbers truncating the quotient, reverts on division by zero.
-  */
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    require(b > 0); // Solidity only automatically asserts when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-
-    return c;
-  }
-
-  /**
-  * @dev Subtracts two numbers, reverts on overflow (i.e. if subtrahend is greater than minuend).
-  */
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    require(b <= a);
-    uint256 c = a - b;
-
-    return c;
-  }
-
-  /**
-  * @dev Adds two numbers, reverts on overflow.
-  */
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    require(c >= a);
-
-    return c;
-  }
-
-  /**
-  * @dev Divides two numbers and returns the remainder (unsigned integer modulo),
-  * reverts when dividing by zero.
-  */
-  function mod(uint256 a, uint256 b) internal pure returns (uint256) {
-    require(b != 0);
-    return a % b;
-  }
-}
-
-/**
  * @title SafeERC20
  * @dev Wrappers around ERC20 operations that throw on failure.
  * To use this library you can add a `using SafeERC20 for ERC20;` statement to your contract,
  * which allows you to call the safe operations as `token.safeTransfer(...)`, etc.
  */
 library SafeERC20 {
-
-  using SafeMath for uint256;
 
   function safeTransfer(
     IERC20 token,
@@ -152,7 +84,7 @@ library SafeERC20 {
   )
     internal
   {
-    uint256 newAllowance = token.allowance(address(this), spender).add(value);
+    uint256 newAllowance = token.allowance(address(this), spender) + value;
     require(token.approve(spender, newAllowance));
   }
 
@@ -163,7 +95,7 @@ library SafeERC20 {
   )
     internal
   {
-    uint256 newAllowance = token.allowance(address(this), spender).sub(value);
+    uint256 newAllowance = token.allowance(address(this), spender) - value;
     require(token.approve(spender, newAllowance));
   }
 }
@@ -187,7 +119,7 @@ contract Ownable {
    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
    * account.
    */
-  constructor() internal {
+  constructor() {
     _owner = msg.sender;
     emit OwnershipTransferred(address(0), _owner);
   }
@@ -249,15 +181,15 @@ contract Ownable {
  * @title TokenVesting
  * @dev A token holder contract that can release its token balance gradually like a
  * typical vesting scheme, with a cliff and vesting period.
+ * 
+ * Note: Deploy one Vesting contract per user
  *
  * Note you do not want to transfer tokens you have withdrawn back to this contract. This will
  * result in some fraction of your transferred tokens being locked up again.
  *
- * Code taken from OpenZeppelin/openzeppelin-solidity at commit 4115686b4f8c1abf29f1f855eb15308076159959.
- * (Revocation options removed by Reserve.)
+ * Updated from Code taken from OpenZeppelin/openzeppelin-solidity at commit 4115686b4f8c1abf29f1f855eb15308076159959.
  */
 contract TokenVesting is Ownable {
-  using SafeMath for uint256;
   using SafeERC20 for IERC20;
 
   event TokensReleased(address token, uint256 amount);
@@ -274,33 +206,39 @@ contract TokenVesting is Ownable {
    * @dev Creates a vesting contract that vests its balance of any ERC20 token to the
    * beneficiary, gradually in a linear fashion until start + duration. By then all
    * of the balance will have vested.
-   * @param beneficiary address of the beneficiary to whom vested tokens are transferred
-   * @param cliffDuration duration in seconds of the cliff in which tokens will begin to vest
-   * @param duration duration in seconds of the period in which the tokens will vest
+   * @param beneficiary_ address of the beneficiary to whom vested tokens are transferred
+   * @param cliffDuration_ duration in seconds of the cliff in which tokens will begin to vest
+   * @param duration_ duration in seconds of the period in which the tokens will vest
    */
   constructor(
-    address beneficiary,
-    uint256 cliffDuration,
-    uint256 duration
+    address beneficiary_,
+    uint256 cliffDuration_,
+    uint256 duration_
   )
-    public
   {
-    require(beneficiary != address(0));
-    require(cliffDuration <= duration);
-    require(duration > 0);
+    require(beneficiary_ != address(0));
+    require(cliffDuration_ <= duration_);
+    require(duration_ > 0);
     
 
-    _beneficiary = beneficiary;
-    _duration = duration;
-    _cliff = block.timestamp.add(cliffDuration);
+    _beneficiary = beneficiary_;
+    _duration = duration_;
+    _cliff = block.timestamp + cliffDuration_;
     _start = block.timestamp;
   }
   
+   /**
+   * @dev Rescind a vesting contract. If the owner of this contract wants to stop vesting they can do it with this function.
+   * When called rescind sends the amount of tokens already vested to the beneficiary and sends the rest back to the owner. 
+   * It also updates the _duration variable to reflect that vesting for this contract has ended. 
+   * Because of this functions structure only a single ERC20 may be vested per contract.
+   * @param token the address of the token to rescind
+   */
   function rescind(IERC20 token) public onlyOwner {
-      uint256 releasableNow = releasable(token);
-      uint256 toRescind = token.balanceOf(address(this)).sub(releasableNow);
+      uint256 releasableNow = releasable(address(token));
+      uint256 toRescind = token.balanceOf(address(this)) - releasableNow;
       token.safeTransfer(owner(), toRescind);
-      _duration = block.timestamp.sub(_start);
+      _duration = block.timestamp - _start;//now _start + duration == block.timestamp
       token.safeTransfer(_beneficiary, releasableNow);
   }
 
@@ -354,12 +292,12 @@ contract TokenVesting is Ownable {
     uint256 unreleased = _releasableAmount(token);
 
     require(unreleased > 0);
-
-    _released[token] = _released[token].add(unreleased);
+    address tokenAddress = address(token);
+    _released[tokenAddress] = _released[tokenAddress] + unreleased;
 
     token.safeTransfer(_beneficiary, unreleased);
 
-    emit TokensReleased(token, unreleased);
+    emit TokensReleased(tokenAddress, unreleased);
   }
 
   /**
@@ -367,7 +305,7 @@ contract TokenVesting is Ownable {
    * @param token ERC20 token which is being vested
    */
   function _releasableAmount(IERC20 token) private view returns (uint256) {
-    return _vestedAmount(token).sub(_released[token]);
+    return _vestedAmount(token) - _released[address(token)];
   }
 
   /**
@@ -375,15 +313,15 @@ contract TokenVesting is Ownable {
    * @param token ERC20 token which is being vested
    */
   function _vestedAmount(IERC20 token) private view returns (uint256) {
-    uint256 currentBalance = token.balanceOf(this);
-    uint256 totalBalance = currentBalance.add(_released[token]);
+    uint256 currentBalance = token.balanceOf(address(this));
+    uint256 totalBalance = currentBalance + _released[address(token)];
 
     if (block.timestamp < _cliff) {
       return 0;
-    } else if (block.timestamp >= _start.add(_duration)) {
+    } else if (block.timestamp >= _start + _duration) {
       return totalBalance;
     } else {
-      return totalBalance.mul(block.timestamp.sub(_start)).div(_duration);
+      return (totalBalance * (block.timestamp - _start)) / _duration;
     }
   }
 }
